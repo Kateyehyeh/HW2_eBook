a1a36e90a99b60b9fff4fea636b928fc70b97e63
Kate Yeh <kateyehyeh@gmail.com>
2024-11-22 11:17:48 +0800

    Initial Commit

---
+++ HW2_eBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2_eBook
+//
+//  Created by Kateyeh on 2024/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
